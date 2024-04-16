942862ce0b9026f7fcbbc08f428bb4e2f7bb4cc7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-16 12:11:26 +0400

    TodoContext provider etdik ve value-ye todo verdik

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,6 +1,14 @@
-import { createContext } from "react";
+import { Children, createContext } from "react";
 const todo ={
     name:"Qasim",
     surname:"Ali",
 }
 export const TodoContext = createContext(todo)
+
+export const TodoContextApp = ({children})=>{
+    return(
+        <TodoContext.Provider value={todo}>
+            {children}
+        </TodoContext.Provider>
+    )
+}
