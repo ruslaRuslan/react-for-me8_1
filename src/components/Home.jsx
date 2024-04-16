9f3d4d5552a6675b0f0ab0cd6cf93b0f7a2dc7cd
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-16 11:27:42 +0400

    Home todoInput & todo

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useContext, useState } from "react";
+import { TodoContext } from "../context/TodoContext";
+
+export const Home = () => {
+  const [todoInput, setTodoInput] = useState();
+  const [todo, setTodo] = useState([]);
+  const addToDo = (e) => {
+    e.preventDefault();
+    console.log(e.target.todoInput.value);
+  };
+  return (
+    <div>
+      <h1>Home</h1>
+      <form onSubmit={addToDo}>
+        <input type="text" placeholder="enter todo" name="todoInput" />
+        <input type="submit" value="add ToDo" />
+      </form>
+    </div>
+  );
+};
