6525d032d97868043d4bd0b9eaa08401f5734bbe
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-16 11:55:46 +0400

    id-ni set eledik

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,21 @@ import { TodoContext } from "../context/TodoContext";
 export const Home = () => {
   const [todoInput, setTodoInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [id, setID]= useState(0)
   const addToDo = (e) => {
     e.preventDefault();
-    // console.log(e.target.todoInput.value);
-    console.log(todoInput)
-    setTodoInput("")
-    setTodos([...todos,{
-        id:2,
-        todoTitle:todoInput
-    }])
-  }
+    setID(id)
+    setTodos([
+      ...todos,
+      {
+        id: id,
+        todoTitle: todoInput,
+      },
+    ]);
+
+    console.log(todos);
+    setTodoInput("");
+  };
   return (
     <div>
       <h1>Home</h1>
